1df6bbd5b9af6f48a69ac20b6d122277de5cf689
Ben McChesney <ben.mcchesney@gmail.com>
2013-10-06 13:32:17 -0700

    Added propr ofxMatrixTransformImage.cpp
    added filePath for ofxMatrixTransformImage for ease of threaded loading.
    
    ofxLoadingManager now has some threaded loading controls as well.

--- src/ofxMatrixTransformImage.h
+++ src/ofxMatrixTransformImage.h
@@ -9,59 +9,22 @@ class ofxMatrixTransformImage : public ofxAlphaStackItem , public ofxMatrixTrans
 	public :
         bool bInheritAlpha ;
 		ofxMatrixTransformImage ( )
-		{
-			scale = 1.0f ;
-			rotation = 0.0f ;
-			bInheritAlpha = false ;
-			//alpha = 0 ;
-		}
-    
+
         ~ofxMatrixTransformImage ( )
-        {
-            image.clear() ; 
-        }
-
-		void loadImage( string path , float _x , float _y , ofPoint anchor , float _alpha = 0.0f )
-		{
-		    image.loadImage( path ) ;
-		    x = _x ;
-		    y = _y ;
-            _anchor = anchor ;
-//		    image.setAnchorPercent( anchor.x , anchor.y ) ;
-            alpha = _alpha ;
-            //cout << "loadImage : " << path << endl ;
-		}
-    
-        ofPoint _anchor ;
-
-        void setImageProeprties( float _x , float _y , ofPoint anchor , float _alpha = 0.0f )
-        {
-            x = _x ;
-            y = _y ;
-            _anchor = anchor ;
-            //image.setAnchorPercent( anchor.x , anchor.y ) ;
-            alpha = _alpha ;
-        }
-    
-		ofImage image ;
-
 		void draw ( )
-		{
-			matrixPush( ) ;
-              //  if ( !bInheritAlpha )
-                ofSetColor ( 255 , 255 , 255 , getOFAlpha() ) ;
-                image.draw ( image.width * -_anchor.x , image.height * -_anchor.y ) ;
-               // image.draw ( image.width * _anchor.x , image.height * _anchor.y ) ;
-			matrixPop( ) ;
-		}
-    
-        void resizePorportionally ( float _width , float _height )
-        {
-            if ( image.bAllocated() )
-            {
-                
-            }
-            
-        }
+		void loadImage( string path , float _x , float _y , ofPoint anchor , float _alpha = 0.0f )
+        void setImageProperties( string path , float _x , float _y , ofPoint anchor , float _alpha = 0.0f  ) 
+		void setImagePath ( string path ) 	
+		bool isNewlyLoaded( ) 
+
+
+		bool bLoaded ; 
+		int resizeWidth , resizeHeight ; 
+		
+		 ofPoint _anchor ;
+		ofImage image ;
+		string imageFilePath ; 
+		
+	
     
 };
